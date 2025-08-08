ac198c5fe97de53b1f9e2ca7ca6de26d90b2b082
Adil Munawar <adilfromavengers@gmail.com>
2025-08-08 23:53:32 +0500

    Update 20250614212547-e9cef2f7-810d-4f75-970b-12a4ed2f66c5.sql

--- supabase/migrations/20250614212547-e9cef2f7-810d-4f75-970b-12a4ed2f66c5.sql
+++ supabase/migrations/20250614212547-e9cef2f7-810d-4f75-970b-12a4ed2f66c5.sql
@@ -1,9 +1,7 @@
-
 -- Add foreign key relationships to activity_logs table
 ALTER TABLE public.activity_logs 
 ADD CONSTRAINT activity_logs_user_id_fkey 
 FOREIGN KEY (user_id) REFERENCES public.profiles(id) ON DELETE CASCADE;
-
 ALTER TABLE public.activity_logs 
 ADD CONSTRAINT activity_logs_company_id_fkey 
 FOREIGN KEY (company_id) REFERENCES public.companies(id) ON DELETE CASCADE;
